6918c5830b48593e23408f78f2ae2f4477cfeeb6
5831063821-WP <i.prakrim@gmail.com>
2016-08-30 14:20:09 +0700

    Implement hello and bubble sort

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -13,12 +13,22 @@ public class Hello {
 
 	public static String greetingMessage(String name){
 		// TODO Return string in this format - "Hello, " + given name.
-		return "";
+		return "Hello, " + name;
 	}
 
 	public static int[] bubbleSort(int[] array){
 		// TODO Sort the given array in an ascending order and return the sorted array.
+		for (int i = 0 ; i< array.length-1 ; i++){
+			for( int j = 0 ; j < array.length - 1; j++){
+				if( array[j] > array[j+1]){
+					int t = array[j];
+					 array[j]= array[j+1];
+					 array[j+1]= t ;
+				}
+			}
+		}
 		return array;
+
 	}
 	
 	public static String arrayToString(String title,int[] array){
